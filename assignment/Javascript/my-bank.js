1f6139651414312890535b5480071d58505454fa
rakibmiah-dms <rakibmiah.dms@gmail.com>
2026-07-19 15:29:10 +0600

    done

--- assignment/Javascript/my-bank.js
+++ assignment/Javascript/my-bank.js
@@ -20,4 +20,30 @@ depositBtn.addEventListener("click", function(){
 
     document.getElementById("current-deposit").innerText = totalDeposit;
     document.getElementById("deposit-amount").value = "";
+
+    const currentBalance = document.getElementById("current-balance").innerText;
+    const currentBalanceNumber = parseFloat(currentBalance);
+    const totalBalance = depositNumber + currentBalanceNumber;
+    document.getElementById("current-balance").innerText = totalBalance;
+
+    
+})
+
+// // withdraw button even handler
+const WithdrawBtn = document.getElementById("add-withdraw");
+WithdrawBtn.addEventListener("click", function(){
+    const withdrawAmount = document.getElementById("withdraw-amount").value;
+    const withdrawNumber = parseFloat(withdrawAmount);
+
+    const currentWithdraw = document.getElementById("current-withdraw").innerText;
+    const currentWithdrawNumber = parseFloat(currentWithdraw);
+    const totalWithdraw = withdrawNumber + currentWithdrawNumber;
+
+    document.getElementById("current-withdraw").innerText = totalWithdraw;
+    document.getElementById("withdraw-amount").value = "";
+
+    const currentBalance = document.getElementById("current-balance").innerText;
+    const currentBalanceNumber = parseFloat(currentBalance);
+    const totalBalance = currentBalanceNumber + -1 * withdrawNumber;
+    document.getElementById("current-balance").innerText = totalBalance;
 })
